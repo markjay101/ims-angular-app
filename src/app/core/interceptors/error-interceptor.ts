d26e88758c7a6c7a7a7a20e20660adc2c709626e
markjay <markjayfuentescuyos1@gmail.com>
2026-01-31 11:43:50 +0800

    add http error interceptor

---
+++ src/app/core/interceptors/error-interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { ApiResponse } from '../../shared/models/api-response';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      const apiResponse = error.error as ApiResponse<any>;
+
+      let errorMessage = 'An unexpected error occurred';
+
+      if (apiResponse?.errors?.length > 0) {
+        errorMessage = apiResponse.errors[0];
+      } else if (error.status === 0) {
+        errorMessage = 'Cannot connect to server. Try again later.';
+      }
+
+      console.error(errorMessage);
+
+      return throwError(() => new Error(errorMessage));
+    }),
+  );
+};
